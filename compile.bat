4eed7418029f5e245e1ed2431c85006b253787ba
Hargallen <b.shaehov@kartli.ru>
2024-07-19 09:48:47 +0300

    upgrade compiler

--- compile.bat
+++ compile.bat
@@ -1,3 +1,11 @@
-cmd /U /k "env\Scripts\activate&&pip install pyinstaller&&pyinstaller main.py"
+cmd /U /k "env\Scripts\activate&&pip install pyinstaller&&pyinstaller main.py&&exit"
 
+xcopy /y /e "ico\*.*" "dist\main\_internal\ico\*.*"
 
+xcopy /y /e "Apache\*.*" "dist\main\Apache\*.*"
+xcopy /y /e "zf_1c_connect_client\*.*" "dist\main\zf_1c_connect_client\*.*"
+xcopy /y /e "extension\*.*" "dist\main\extension\*.*"
+
+copy "PLINK.EXE" "dist\main\PLINK.EXE"
+
+PAUSE
