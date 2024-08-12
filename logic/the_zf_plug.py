c3507edf93fdf2e6f746b333569fe11133223649
mashina <breadwe@gmail.com>
2024-08-12 17:03:54 +0300

    creating folders with zf

--- logic/the_zf_plug.py
+++ logic/the_zf_plug.py
@@ -1,4 +1,6 @@
 import json
+import os
+import shutil
 import subprocess
 from settings import the_path_to_zf
 
@@ -29,3 +31,26 @@ def json_settings():
 def zf_plug():
     command = f'cd {the_path_to_zf} && zf_connector.exe'
     subprocess.run(['cmd', '/c', 'start', 'cmd', '/k', command])
+
+
+def create_file_folder_zf():
+    base_path = r"C:\zf_connector\base_"  # Используем сырую строку
+    source_dir = 'output'
+
+    i = 1
+
+    while True:
+        path = f'{base_path}{i}'
+        if not os.path.exists(path):
+            os.mkdir(path)
+            print(f'Создана папка: {path}')
+
+            for item in os.listdir(source_dir):
+                s = os.path.join(source_dir, item)
+                d = os.path.join(path, item)
+                if os.path.isdir(s):
+                    shutil.copytree(s, d, dirs_exist_ok=True)
+                else:
+                    shutil.copy2(s, d)
+            break
+        i += 1
